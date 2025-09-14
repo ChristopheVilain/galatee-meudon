5e7076904a16b53c020f332d45ac164285bd4069
ChristopheVilain <christophe.vilain@taster.com>
2025-09-14 21:53:09 +0200

    chore(interface): gestion footer et css, navigation revue

---
+++ interface/app/composants/footer/footer.tsx
@@ -0,0 +1,15 @@
+export function Footer() {
+    return (
+        <footer className="galatee-footer">
+            <div className="galatee-footer-mentions-legales">
+                <p>Mentions légales</p>
+            </div>
+            <div className="galatee-footer-contact">
+                <p>Contact</p>
+            </div>
+            <div className="galatee-footer-copyright">
+                <p>Copyright 2025 Galatée Meudon</p>
+            </div>
+        </footer>
+    )
+}
--- interface/app/pages/page_principale/bienvenue.tsx
+++ interface/app/pages/page_principale/bienvenue.tsx
@@ -1,5 +1,6 @@
-import { Header } from "../../composants/footer/header";
+import { Header } from "../../composants/header/header";
 import { Navbar } from "../../composants/nav/navbar";
+import { Footer } from "../../composants/footer/footer";
 
 export function Bienvenue() {
     return (
@@ -8,12 +9,35 @@ export function Bienvenue() {
             <Navbar />
             <main className="conteneur-principal">
                 <div className="div-principale">
-                    <h1>Bienvenue sur le site web du club de plongée Galatée Meudon !</h1>
+                    <h1 className="titre-div-principale">Bienvenue sur le site web du club de plongée Galatée Meudon !</h1>
                 </div>
                 <div className="div-secondaire">
-                    Nous sommes un club de plongée qui a été fondé en 1975.
+                    
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis ipsum eget lorem tristique viverra. Phasellus rutrum nisi at odio condimentum interdum. Mauris imperdiet purus ac mauris luctus aliquet. Aliquam laoreet malesuada condimentum. Aliquam id orci auctor, malesuada urna a, convallis diam. Pellentesque aliquet urna diam, eget venenatis odio viverra quis. Phasellus sagittis lorem a accumsan mollis. Duis quis enim pulvinar, vulputate augue id, porta ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas nec ex tellus. Sed egestas, nibh quis consectetur porta, nulla augue condimentum magna, at interdum mi ex id tellus. Donec sapien felis, bibendum at finibus a, sollicitudin fermentum felis. Quisque pharetra iaculis massa a porta.
+
+Maecenas vel mi non velit maximus varius. Maecenas in arcu ex. Nulla dapibus aliquam laoreet. Ut vitae hendrerit ex. Nam pellentesque dapibus suscipit. Proin augue massa, fringilla non sapien eu, dapibus faucibus mi. Duis ac tellus purus. Nam tortor lacus, tempus quis dapibus eget, dapibus at purus. Phasellus maximus vulputate erat vitae posuere. Morbi quis commodo eros.
+
+Nulla sed quam nec tortor ultrices rutrum a a enim. Donec at justo ornare, consequat nisl non, sollicitudin lacus. Suspendisse cursus ut neque ut egestas. Integer eget dui malesuada, porta sapien eu, elementum massa. Proin quis sapien eget diam mollis porttitor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus a nulla eget urna pretium faucibus eu tincidunt nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum quam quis vehicula sollicitudin. Quisque ut sodales ante, sed lacinia orci. Curabitur interdum semper lacus, at semper nunc ultrices a. Aliquam ultricies nec mauris vitae convallis. Ut eleifend dui at cursus tempus.
+
+Praesent finibus erat eget nisi interdum ornare. Pellentesque et tempor diam. Nulla facilisis commodo iaculis. Fusce rutrum, diam in varius imperdiet, justo augue pellentesque sapien, quis pharetra nisl metus sit amet sapien. Duis rutrum orci eget erat lacinia, nec ultricies lacus interdum. Vestibulum tristique a metus sit amet mattis. Curabitur ac facilisis ante. Morbi pellentesque, nulla sit amet pharetra placerat, eros velit imperdiet mi, ut euismod dui nibh eleifend metus. Vivamus neque sapien, euismod eu efficitur at, hendrerit ut enim. Etiam urna enim, pulvinar nec metus ut, molestie interdum urna. Maecenas scelerisque lorem non neque iaculis lobortis.
+
+Proin eu justo ut arcu ultricies lacinia. Ut eleifend lacus massa, sit amet interdum felis fermentum vitae. Sed eu ullamcorper justo. In posuere ornare nibh, in egestas elit rhoncus id. Etiam quis placerat dui, non semper eros. Nulla facilisi. Donec ac orci leo. Morbi pellentesque, erat eu consequat mollis, urna mi venenatis dui, id sagittis diam quam mollis est. Curabitur egestas porta neque eu consectetur. Maecenas fermentum eget metus ac accumsan. Curabitur pulvinar diam in feugiat mattis. Vestibulum sit amet felis sit amet dolor euismod interdum nec ac massa. 
+
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis ipsum eget lorem tristique viverra. Phasellus rutrum nisi at odio condimentum interdum. Mauris imperdiet purus ac mauris luctus aliquet. Aliquam laoreet malesuada condimentum. Aliquam id orci auctor, malesuada urna a, convallis diam. Pellentesque aliquet urna diam, eget venenatis odio viverra quis. Phasellus sagittis lorem a accumsan mollis. Duis quis enim pulvinar, vulputate augue id, porta ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas nec ex tellus. Sed egestas, nibh quis consectetur porta, nulla augue condimentum magna, at interdum mi ex id tellus. Donec sapien felis, bibendum at finibus a, sollicitudin fermentum felis. Quisque pharetra iaculis massa a porta.
+
+Maecenas vel mi non velit maximus varius. Maecenas in arcu ex. Nulla dapibus aliquam laoreet. Ut vitae hendrerit ex. Nam pellentesque dapibus suscipit. Proin augue massa, fringilla non sapien eu, dapibus faucibus mi. Duis ac tellus purus. Nam tortor lacus, tempus quis dapibus eget, dapibus at purus. Phasellus maximus vulputate erat vitae posuere. Morbi quis commodo eros.
+
+Nulla sed quam nec tortor ultrices rutrum a a enim. Donec at justo ornare, consequat nisl non, sollicitudin lacus. Suspendisse cursus ut neque ut egestas. Integer eget dui malesuada, porta sapien eu, elementum massa. Proin quis sapien eget diam mollis porttitor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus a nulla eget urna pretium faucibus eu tincidunt nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum quam quis vehicula sollicitudin. Quisque ut sodales ante, sed lacinia orci. Curabitur interdum semper lacus, at semper nunc ultrices a. Aliquam ultricies nec mauris vitae convallis. Ut eleifend dui at cursus tempus.
+
+Praesent finibus erat eget nisi interdum ornare. Pellentesque et tempor diam. Nulla facilisis commodo iaculis. Fusce rutrum, diam in varius imperdiet, justo augue pellentesque sapien, quis pharetra nisl metus sit amet sapien. Duis rutrum orci eget erat lacinia, nec ultricies lacus interdum. Vestibulum tristique a metus sit amet mattis. Curabitur ac facilisis ante. Morbi pellentesque, nulla sit amet pharetra placerat, eros velit imperdiet mi, ut euismod dui nibh eleifend metus. Vivamus neque sapien, euismod eu efficitur at, hendrerit ut enim. Etiam urna enim, pulvinar nec metus ut, molestie interdum urna. Maecenas scelerisque lorem non neque iaculis lobortis.
+
+Proin eu justo ut arcu ultricies lacinia. Ut eleifend lacus massa, sit amet interdum felis fermentum vitae. Sed eu ullamcorper justo. In posuere ornare nibh, in egestas elit rhoncus id. Etiam quis placerat dui, non semper eros. Nulla facilisi. Donec ac orci leo. Morbi pellentesque, erat eu consequat mollis, urna mi venenatis dui, id sagittis diam quam mollis est. Curabitur egestas porta neque eu consectetur. Maecenas fermentum eget metus ac accumsan. Curabitur pulvinar diam in feugiat mattis. Vestibulum sit amet felis sit amet dolor euismod interdum nec ac massa. 
                 </div>
             </main>
+            <Footer />
         </>
     )
 }
